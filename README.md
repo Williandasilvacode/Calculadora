beabebd94822e67dded6b3d53288e6c02b8cd326
Willian da Silva <78602442+Williandasilvacode@users.noreply.github.com>
2021-06-05 12:31:35 -0300

    commitando o código

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
 # Calculadora
- Essa é uma calculadora simples, criada com a linguagem de programação Python.
+ Essa é uma calculadora simples, criada com a linguagem de programação Python, utilizando laço de repetição "while", utilizei também, "break" para encerra o laço e "continue" para continua o laço.
---
+++ index.py
@@ -0,0 +1,38 @@
+﻿print('\n <<------Calculadora Simples----->>')
+print('+ Adição')
+print('- Subtração')
+print('* Multiplicação')
+print('/ Divisão')
+print('Presione a tecla "s" para encerra o programa!')
+
+
+# (\n) faz uma quebra de linha
+while True:
+    op = input('\n Qual operação deseja fazer? ')
+    if op == '+' or op =='-' or op =='*' or op =='/':
+        x = float(input('Digite o primeiro número:'))
+        y = float(input('Digite o segundo número:'))
+
+    if (op == '+'):
+        res = x + y
+        print('\n Resultado: {} + {} = {}'.format(x, y, res))
+        continue
+    elif (op == '-'):
+        res = x - y
+        print('\n Resultado: {} - {} = {}'.format(x, y, res))
+        continue
+    elif (op == '*'):
+        res = x * y
+        print('\n Resultado: {} * {} = {}'.format(x, y, res))
+        continue
+    elif (op == '/'):
+        res = x / y
+        print('\n Resultado: {} / {} = {}'.format(x, y, res))
+        continue
+    elif (op == 's'):
+        break
+    else:
+        print('\n Operação invalida!')
+
+
+print('\n Encerrando o programa...!')
